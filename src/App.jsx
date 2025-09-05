a1736540a5aef73291c33a42d9454f0bac597362
“rishabhvyass” <rishabhvyas125@gmail.com>
2025-09-06 04:29:11 +0530

    make Admin Dashboard and ui of that and add routing to dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 import "./App.css";
+import { Header } from "./components/Students/Header";
+import "./components/Students/Header/style.css";
+import "./components/Admin/layout/Topbar/style.css";
+import "./components/Admin/layout/sidebar/style.css";
+import "./components/Admin/AdminDashboard/style.css";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./components/Admin/layout/topbar";
+import Sidebar from "./components/Admin/layout/sidebar";
+import AdminDashboard from "./components/Admin/AdminDashboard";
 
 function App() {
   return (
     <>
-      <div>Hi there</div>
+      <Topbar />
+      <div className="conta">
+        <div className="main-content">
+          <Routes>
+            <Route path="/dashborad" element={<AdminDashboard />} />
+            <Route path="/Sidebar" element={<Sidebar />} />
+          </Routes>
+        </div>
+
+        <div className="app-container">
+          <Sidebar />
+        </div>
+      </div>
     </>
   );
 }
---
+++ src/components/Students/Header/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div className="mainContainer">
+      <div className="logo"> StudentArc </div>
+    </div>
+  );
+};
