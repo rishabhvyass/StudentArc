0f2d78f0313b8b0b9e8e9d7bed043acaad3a1214
vishwaraj00346 <vishwarajsinghharod@gmail.com>
2025-09-07 04:21:11 +0530

    Create  Dashboard and Event page

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,20 @@
 import React from "react";
 import "./App.css";
-import Login from "./pages/login/index"
-import './pages/login/style.css'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Home } from "./pages/home";
 import Events from "./pages/events/index";
 import Dashboard from "./pages/dashboard/index";
 
 function App() {
 
   const router = createBrowserRouter([
-    {
-      path:"/",
-      element:<Login/>
 
+    {
+      path: "events",
+      element: <Events />
     },
     {
-      path:"home",
-      element:<Home/>
-    },
-    {
-      path:"events",
-      element:<Events/>
-    },
-    {
-      path:"dashboard",
-      element:<Dashboard/>
+      path: "dashboard",
+      element: <Dashboard />
     }
 
   ])
